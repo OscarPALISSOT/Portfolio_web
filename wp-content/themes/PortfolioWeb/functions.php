3280063691fd5981634b3d8437e968d0cfa33eae
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2022-09-25 02:14:31 +0200

    test register style

--- wp-content/themes/PortfolioWeb/functions.php
+++ wp-content/themes/PortfolioWeb/functions.php
@@ -1,2 +1,19 @@
 <?php
-add_theme_support('title-tag');
+
+function theme_support () {
+	add_theme_support('title-tag');
+}
+
+function theme_register_assets()
+{
+	wp_register_style('style', get_stylesheet_uri(), []);
+	wp_enqueue_style('style');
+}
+
+function theme_title_separator (): string {
+	return '|';
+}
+
+add_action('wp_enqueue_scripts', 'theme_register_assets');
+add_action('after_setup_theme', 'theme_support');
+add_filter('document_title_separator', 'theme_title_separator');
--- wp-content/themes/PortfolioWeb/index.php
+++ wp-content/themes/PortfolioWeb/index.php
@@ -1,6 +1,11 @@
 <?php get_header() ?>
 
-    hello world
+    <div class="name">
+        <h1>Oscar</h1>
+        <h1>PALISSOT</h1>
+    </div>
+    <p>hello world</p>
+
 
 <?php get_footer() ?>
 

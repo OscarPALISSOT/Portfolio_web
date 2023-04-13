5bd56c04c819a7c433f936d9cb51f7cc78502a3c
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-04-14 00:49:50 +0200

    footer social logo effect

--- wp-content/themes/PortfolioWeb/assets/js/footer.js
+++ wp-content/themes/PortfolioWeb/assets/js/footer.js
@@ -9,4 +9,24 @@ window.onload = function (){
         link.appendChild(after);
     }
 
+    const socialLinks = document.getElementsByClassName('social__link');
+    for (let i=0; i<socialLinks.length; i++){
+        let link = socialLinks[i];
+        let x = link.offsetLeft + link.offsetWidth/2;
+        let y = link.offsetTop - link.offsetHeight/2;
+        link.addEventListener('mousemove', function (e){
+            let xCor = e.clientX - x
+            let yCor = e.screenY - y;
+            if (Math.abs(xCor) > 50 || Math.abs(yCor) > 50){
+                let moveX = - xCor + link.offsetWidth/2;
+                let moveY = - yCor - link.offsetHeight/2;
+                console.log(moveX, moveY);
+                link.style.transform = 'translate(' + moveX + 'px, ' + moveY + 'px)'
+            }else{
+                link.style.transform = 'translate(' + xCor + 'px, ' + yCor + 'px)';
+            }
+
+        })
+    }
+
 }
--- wp-content/themes/PortfolioWeb/widgets/socials.php
+++ wp-content/themes/PortfolioWeb/widgets/socials.php
@@ -14,7 +14,7 @@ foreach ( $networks as $network => $label ) {
 	if ( ! empty( $instance[ $network ] ) ) {
 		?>
 
-        <a href=" <?= esc_url( $instance[ $network ] ) ?>">
+        <a href=" <?= esc_url( $instance[ $network ] ) ?>" class="social__link">
             <img class="socials__icons" src="<?= get_template_directory_uri() . '/assets/img/' . $label . '_icon.svg'?>" alt="<?= $label?>">
         </a>
 		<?php

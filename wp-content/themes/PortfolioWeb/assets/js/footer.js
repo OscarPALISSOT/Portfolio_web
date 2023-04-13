425e94d527740eae8f7c8f4cc735006f09dbb197
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-04-13 15:54:15 +0200

    roll effect footer

--- wp-content/themes/PortfolioWeb/assets/js/footer.js
+++ wp-content/themes/PortfolioWeb/assets/js/footer.js
@@ -1,10 +1,12 @@
 window.onload = function (){
     const links = document.getElementsByClassName('footerCol')[1].getElementsByTagName('ul')[0].getElementsByTagName('a');
-    for(let i = 0; i < links.length; i++){
-        const afterElement = document.createElement('span');
-        const texteElement = links[i].textContent;
-        afterElement.style.content = "'" + texteElement + "'";
-        links[i].appendChild(afterElement);
+    for (let i=0; i<links.length; i++){
+        let link = links[i];
+        let text = link.innerHTML;
+        let after = document.createElement('span');
+        after.classList.add('afterLinks');
+        after.innerHTML = text;
+        link.appendChild(after);
     }
 
 }

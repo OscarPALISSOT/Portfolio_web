17d8a63fce47b868fb7ca38e94a62e2d1c6935d7
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-05-01 21:14:53 +0200

    fix motion effect socials links

--- wp-content/themes/PortfolioWeb/assets/js/footer.js
+++ wp-content/themes/PortfolioWeb/assets/js/footer.js
@@ -12,18 +12,20 @@ window.onload = function (){
     }
 
     // motion effect
+
     const socialLinks = document.getElementsByClassName('social__link');
     const motionZone = {
-        x: 50,
-        y:30
+        x: 100,
+        y:40
     }
     for (let i=0; i<socialLinks.length; i++){
         let link = socialLinks[i];
         let x = link.offsetLeft + link.offsetWidth/2;
-        let y = link.offsetTop - link.offsetHeight/2;
+        let y = link.offsetTop + link.offsetHeight/2
         link.addEventListener('mousemove', function (e){
-            let xCor = e.clientX - x
-            let yCor = e.screenY - y;
+            let xCor = e.pageX - x
+            let yCor = e.pageY - y;
+            console.log('move:', xCor, yCor)
             if (Math.abs(xCor) > motionZone.x || Math.abs(yCor) > motionZone.y){
                 link.style.transform = 'none'
             }else{

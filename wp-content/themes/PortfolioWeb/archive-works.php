3f06a8284d96b671f7c5d7be6ed8266c8ff3d8d1
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-05-15 00:11:11 +0200

    debut archive works

--- wp-content/themes/PortfolioWeb/archive-works.php
+++ wp-content/themes/PortfolioWeb/archive-works.php
@@ -1,8 +1,19 @@
 <?php get_header() ?>
 
-<?php while (have_posts()): the_post() ?>
+<?php /* while (have_posts()): the_post() ?>
     <a href="<?= the_permalink() ?>"><?= esc_attr(get_the_title()) ?></a>
-<?php endwhile; ?>
+<?php endwhile; */?>
+
+<div class="gallery">
+    <div class="work__item"><img src="https://picsum.photos/500/400" alt=""></div>
+    <div class="work__item"><img src="https://picsum.photos/500/400" alt=""></div>
+    <div class="work__item"><img src="https://picsum.photos/500/400" alt=""></div>
+    <div class="work__item"><img src="https://picsum.photos/500/400" alt=""></div>
+    <div class="work__item"><img src="https://picsum.photos/500/400" alt=""></div>
+    <div class="work__item"><img src="https://picsum.photos/500/400" alt=""></div>
+    <div class="work__item"><img src="https://picsum.photos/500/400" alt=""></div>
+</div>
+
 
 <?php get_footer() ?>
 
--- wp-content/themes/PortfolioWeb/include/assets.php
+++ wp-content/themes/PortfolioWeb/include/assets.php
@@ -6,10 +6,12 @@ add_action('wp_enqueue_scripts',  function (){
 	wp_register_style('style', get_template_directory_uri() . '/assets/css/style.css', ['variables', 'font']);
 	wp_register_style('footer', get_template_directory_uri() . '/assets/css/footer.css', ['variables', 'style']);
 	wp_register_style('header', get_template_directory_uri() . '/assets/css/header.css', ['variables', 'style']);
+	wp_register_style('archive-works', get_template_directory_uri() . '/assets/css/archive-works.css', ['variables', 'style']);
 	wp_register_style('font', 'https://fonts.googleapis.com/css2?family=Inter:wght@100..900', []);
 
     wp_enqueue_style('header');
 	wp_enqueue_style('footer');
+    is_post_type_archive('works') && wp_enqueue_style('archive-works');
 
     wp_enqueue_script('footerJs', get_template_directory_uri() . '/assets/js/footer.js', [], false, true);
     wp_enqueue_script('headerJs', get_template_directory_uri() . '/assets/js/header.js', [], false, true);

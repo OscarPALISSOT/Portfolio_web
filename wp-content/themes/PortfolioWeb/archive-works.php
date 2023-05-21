671049d8fe70ec6d13f821c62f18b3e0e4d040bb
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-05-22 00:38:33 +0200

    works positions

--- wp-content/themes/PortfolioWeb/archive-works.php
+++ wp-content/themes/PortfolioWeb/archive-works.php
@@ -4,14 +4,44 @@
     <a href="<?= the_permalink() ?>"><?= esc_attr(get_the_title()) ?></a>
 <?php endwhile; */?>
 
+<h1 class="title">
+    <?= get_the_archive_title() ?>
+</h1>
+
 <div class="gallery">
-    <div class="work__item"><img src="https://picsum.photos/500/400" alt=""></div>
-    <div class="work__item"><img src="https://picsum.photos/500/400" alt=""></div>
-    <div class="work__item"><img src="https://picsum.photos/500/400" alt=""></div>
-    <div class="work__item"><img src="https://picsum.photos/500/400" alt=""></div>
-    <div class="work__item"><img src="https://picsum.photos/500/400" alt=""></div>
-    <div class="work__item"><img src="https://picsum.photos/500/400" alt=""></div>
-    <div class="work__item"><img src="https://picsum.photos/500/400" alt=""></div>
+    <div class="left__gallery">
+        <div class="work__item first__work__item"><h1>Lorem ipsum dolor sit amet</h1></div>
+        <div class="work__item">
+            <img src="https://picsum.photos/600/400" alt="">
+            <h2 class="work__item__title">Un projet</h2>
+        </div>
+        <div class="work__item">
+            <img src="https://picsum.photos/600/400" alt="">
+            <h2 class="work__item__title">Un projet</h2>
+        </div>
+        <div class="work__item">
+            <img src="https://picsum.photos/600/400" alt="">
+            <h2 class="work__item__title">Un projet</h2>
+        </div>
+    </div>
+    <div class="right__gallery">
+        <div class="work__item">
+            <img src="https://picsum.photos/600/400" alt="">
+            <h2 class="work__item__title">Un projet</h2>
+        </div>
+        <div class="work__item">
+            <img src="https://picsum.photos/600/400" alt="">
+            <h2 class="work__item__title">Un projet</h2>
+        </div>
+        <div class="work__item">
+            <img src="https://picsum.photos/600/400" alt="">
+            <h2 class="work__item__title">Un projet</h2>
+        </div>
+        <div class="work__item">
+            <img src="https://picsum.photos/600/400" alt="">
+            <h2 class="work__item__title">Un projet</h2>
+        </div>
+    </div>
 </div>
 
 
--- wp-content/themes/PortfolioWeb/include/apparence.php
+++ wp-content/themes/PortfolioWeb/include/apparence.php
@@ -14,3 +14,21 @@ add_action('customize_register', function (WP_Customize_Manager $manager){
 		'section' => 'portfolio_appearance'
 	]));
 });
+
+function my_theme_archive_title( $title ) {
+    if ( is_category() ) {
+        $title = single_cat_title( '', false );
+    } elseif ( is_tag() ) {
+        $title = single_tag_title( '', false );
+    } elseif ( is_author() ) {
+        $title = '<span class="vcard">' . get_the_author() . '</span>';
+    } elseif ( is_post_type_archive() ) {
+        $title = post_type_archive_title( '', false );
+    } elseif ( is_tax() ) {
+        $title = single_term_title( '', false );
+    }
+
+    return $title;
+}
+
+add_filter( 'get_the_archive_title', 'my_theme_archive_title' );

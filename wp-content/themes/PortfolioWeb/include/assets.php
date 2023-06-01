1975ea20f5cced394296d68dfe8fb3080fd9bd79
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-06-02 00:37:03 +0200

    header menu test variable height

--- wp-content/themes/PortfolioWeb/include/assets.php
+++ wp-content/themes/PortfolioWeb/include/assets.php
@@ -7,7 +7,7 @@ add_action('wp_enqueue_scripts',  function (){
 	wp_register_style('footer', get_template_directory_uri() . '/assets/css/footer.css', ['variables', 'style']);
 	wp_register_style('header', get_template_directory_uri() . '/assets/css/header.css', ['variables', 'style']);
 	wp_register_style('archive-works', get_template_directory_uri() . '/assets/css/archive-works.css', ['variables', 'style']);
-	wp_register_style('font', 'https://fonts.googleapis.com/css2?family=Inter:wght@100..900', []);
+	wp_register_style('font', 'https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,slnt,wdth,GRAD,XTRA,YOPQ,YTAS,YTDE,YTFI,YTLC,YTUC@8..144,-10..0,25..151,-200..150,323..603,25..135,649..854,-305..-98,560..788,416..570,528..760', []);
 
     wp_enqueue_style('header');
 	wp_enqueue_style('footer');

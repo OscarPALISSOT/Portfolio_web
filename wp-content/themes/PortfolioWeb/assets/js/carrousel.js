ac23071c1cc0c5c9660953c55710e8ae162dc69a
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-06-14 23:54:33 +0200

    carrousel navigation

--- wp-content/themes/PortfolioWeb/assets/js/carrousel.js
+++ wp-content/themes/PortfolioWeb/assets/js/carrousel.js
@@ -15,22 +15,68 @@ class Carrousel {
             slidesVisible: 1,
         }, options)
 
-        this.children = [].slice.call(element.children);
-        let ratio = this.children.length / this.options.slidesVisible;
+        let children = [].slice.call(element.children);
+        this.currentItem = 0;
 
-        let root = this.createDivWithClass('carrousel');
-        let container = this.createDivWithClass('carrousel__container');
-        container.style.width = (ratio * 100) + "%";
+        this.root = this.createDivWithClass('carrousel');
+        this.container = this.createDivWithClass('carrousel__container');
 
-        root.appendChild(container);
-        this.element.appendChild(root);
+        this.root.appendChild(this.container);
+        this.element.appendChild(this.root);
 
-        this.children.forEach((child) => {
+        this.items = children.map((child) => {
             let item = this.createDivWithClass('carrousel__item');
             item.appendChild(child);
-            container.appendChild(item);
+            this.container.appendChild(item);
+            return item;
         })
+        this.setStyle();
+        this.createNavigation();
     }
+
+
+    /**
+     * Applique les bonnes dimensions aux éléments du carrousel
+     */
+    setStyle() {
+        let ratio = this.items.length / this.options.slidesVisible;
+        this.container.style.width = (ratio * 100) + "%";
+        this.items.forEach(item => item.style.width = ((100 / this.options.slidesVisible) / ratio) + "%");
+    }
+
+    createNavigation() {
+        let nextButton = this.createDivWithClass('carrousel__next');
+        let prevButton = this.createDivWithClass('carrousel__prev');
+        this.root.appendChild(nextButton);
+        this.root.appendChild(prevButton);
+        nextButton.addEventListener('click', this.next.bind(this));
+        prevButton.addEventListener('click', this.prev.bind(this));
+    }
+
+    next() {
+        this.goToItem(this.currentItem + this.options.slidesToScroll);
+    }
+
+    prev() {
+        this.goToItem(this.currentItem - this.options.slidesToScroll);
+    }
+
+    /**
+     * Déplace le carrousel vers l'élément ciblé
+     * @param {number} index
+     */
+    goToItem(index) {
+        if (index < 0) {
+            index = this.items.length - this.options.slidesVisible;
+        } else if ( index >= this.items.length || this.items[this.currentItem + this.options.slidesVisible] === undefined) {
+            index = 0;
+        }
+
+        let translateX = index * -100 / this.items.length;
+        this.container.style.transform = 'translate3d(' + translateX + '%, 0, 0)';
+        this.currentItem = index
+    }
+
 
     /**
      *
@@ -49,6 +95,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     new Carrousel(document.querySelector('.wp-block-gallery'), {
         slidesToScroll: 1,
-        slidesVisible: 3
+        slidesVisible: 2
     });
 });

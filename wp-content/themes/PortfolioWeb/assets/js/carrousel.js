51baffd9ddc0cf16a9c2657f6e45c789cfba90bc
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2023-07-11 00:00:17 +0200

    carrousel pagination

--- wp-content/themes/PortfolioWeb/assets/js/carrousel.js
+++ wp-content/themes/PortfolioWeb/assets/js/carrousel.js
@@ -6,6 +6,8 @@ class Carrousel {
      * @param {Object} options
      * @param {Object} options.slidesToScroll Nombre d'éléments à faire défiler
      * @param {Object} options.slidesVisible Nombre d'éléments visible dans un slide
+     * @param {boolean} options.pagination Affiche la pagination
+     * @param {boolean} options.loop Boucle en fin de carrousel
      */
     constructor(element, options = {}) {
 
@@ -13,6 +15,8 @@ class Carrousel {
         this.options = Object.assign({}, {
             slidesToScroll: 1,
             slidesVisible: 1,
+            pagination: false,
+            loop: false
         }, options)
 
         let children = [].slice.call(element.children);
@@ -27,6 +31,8 @@ class Carrousel {
         this.root.appendChild(this.container);
         this.element.appendChild(this.root);
 
+        this.moveCallbacks = [];
+
         this.items = children.map((child) => {
             let item = this.createDivWithClass('carrousel__item');
             item.appendChild(child);
@@ -35,14 +41,16 @@ class Carrousel {
         })
         this.setStyle();
         this.createNavigation();
+        this.options.pagination && this.createPagination();
+        this.moveCallbacks.forEach(cb => cb(0));
         this.onResize();
         window.addEventListener('resize', this.onResize.bind(this));
 
         // Keyboard navigation
         this.root.addEventListener('keyup', (e) => {
-            if (e.key === 'ArrowLeft'){
+            if (e.key === 'ArrowLeft') {
                 this.prev();
-            } else if (e.key === 'ArrowRight'){
+            } else if (e.key === 'ArrowRight') {
                 this.next();
             }
         })
@@ -58,6 +66,9 @@ class Carrousel {
         this.items.forEach(item => item.style.width = ((100 / this.slidesVisible) / ratio) + "%");
     }
 
+    /**
+     * Crée les boutons de navigation dans le DOM
+     */
     createNavigation() {
         let nextButton = this.createDivWithClass('carrousel__next');
         let prevButton = this.createDivWithClass('carrousel__prev');
@@ -65,6 +76,43 @@ class Carrousel {
         this.root.appendChild(prevButton);
         nextButton.addEventListener('click', this.next.bind(this));
         prevButton.addEventListener('click', this.prev.bind(this));
+        if (this.options.loop === true) {
+            return
+        }
+        this.onMove(index => {
+            if (index === 0) {
+                prevButton.classList.add('carrousel__prev--hidden');
+            } else {
+                prevButton.classList.remove('carrousel__prev--hidden');
+            }
+            if (this.items[this.currentItem + this.slidesVisible] === undefined) {
+                nextButton.classList.add('carrousel__next--hidden');
+            } else {
+                nextButton.classList.remove('carrousel__next--hidden');
+            }
+        })
+    }
+
+    /**
+     * Crée la pagination dans le DOM
+     */
+    createPagination() {
+        let pagination = this.createDivWithClass('carrousel__pagination');
+        let buttons = [];
+        this.root.appendChild(pagination);
+        for (let i = 0; i < (this.items.length - this.slidesVisible) + 1; i = i + this.options.slidesToScroll) {
+            let button = this.createDivWithClass('carrousel__pagination__button')
+            button.addEventListener('click', () => this.goToItem(i))
+            pagination.appendChild(button)
+            buttons.push(button)
+        }
+        this.onMove(index => {
+          let activeButton = buttons[Math.floor(index / this.options.slidesToScroll)];
+          if (activeButton){
+              buttons.forEach(button => button.classList.remove('carrousel__pagination__button--active'));
+              activeButton.classList.add('carrousel__pagination__button--active');
+          }
+        })
     }
 
     next() {
@@ -89,6 +137,16 @@ class Carrousel {
         let translateX = index * -100 / this.items.length;
         this.container.style.transform = 'translate3d(' + translateX + '%, 0, 0)';
         this.currentItem = index
+        this.moveCallbacks.forEach(cb => cb(index));
+    }
+
+
+    /**
+     * Est appelée à chaque déplacement du carrousel
+     * @param cb
+     */
+    onMove(cb) {
+        this.moveCallbacks.push(cb);
     }
 
     onResize() {
@@ -133,5 +191,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     new Carrousel(document.querySelector('.wp-block-gallery'), {
         slidesToScroll: 1,
         slidesVisible: 2,
+        pagination: true,
+        loop: true,
     });
 });

650148d16cbd616717174cbd50e86b23e9fa2872
OscarPALISSOT <oscar.p25000@gmail.com>
2024-12-27 00:12:43 +0100

    update footer

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
 
     return (
         <footer className={'border-t border-text h-24 2xl:px-64 2xl:-mx-64 -mx-24 px-24 flex justify-end items-center'}>
-            <div className={'h-full ps-3 border-l border-l-text flex items-center'}>
+            <div className={'h-full px-3 border-x-0 md:border-x border-x-text flex items-center'}>
                 <p className={'text-base md:text-sm lg:text-lg font-extralight'}>{`© ${currentYear} Oscar PALISSOT. Tous droits réservés.`}</p>
             </div>
         </footer>

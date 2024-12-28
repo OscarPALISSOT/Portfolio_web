aa4d33d76c4a72ab87ff367f55a63c5e8058bd61
OscarPALISSOT <oscar.p25000@gmail.com>
2024-12-29 00:30:19 +0100

    fix work scroll

--- frontend/src/components/works/work.tsx
+++ frontend/src/components/works/work.tsx
@@ -9,10 +9,9 @@ interface WorkProps {
     work: WorkType;
     isOdd?: boolean;
     isFirst?: boolean;
-    isLast?: boolean;
 }
 
-const Work = ({work, isOdd = false, isFirst = false, isLast = false}: WorkProps) => {
+const Work = ({work, isOdd = false, isFirst = false}: WorkProps) => {
 
     const workWrapperRef = useRef<HTMLDivElement>(null);
     const workRef = useRef<HTMLDivElement>(null);
@@ -29,7 +28,7 @@ const Work = ({work, isOdd = false, isFirst = false, isLast = false}: WorkProps)
     }, [isOdd, isFirst]);
 
     return (
-        <div className={`mb-3 md:m-0 border border-background dark:border-text w-full overflow-hidden ${isOdd && !isLast && 'md:border-r-0'} ${isFirst ? 'md:border-t' : 'md:border-t-0'}`} ref={workWrapperRef}>
+        <div className={`mb-3 md:m-0 border border-background dark:border-text w-full overflow-hidden ${isOdd && 'md:border-r-0'} ${isFirst ? 'md:border-t' : 'md:border-t-0'}`} ref={workWrapperRef}>
             <div ref={workRef}>
                 <Image
                     className={'h-full w-full object-cover'}
--- frontend/src/components/works/workGallery.tsx
+++ frontend/src/components/works/workGallery.tsx
@@ -12,29 +12,37 @@ interface WorkGalleryProps {
 
 const WorkGallery = ({workBlock}:WorkGalleryProps) => {
 
+    const workGalleryRef = useRef<HTMLDivElement>(null);
     const titleRef = useRef<HTMLDivElement>(null);
     const descriptionRef = useRef<HTMLDivElement>(null);
+    const leftWorksRef = useRef<HTMLDivElement>(null);
     const rightWorksRef = useRef<HTMLDivElement>(null);
+    const borderRef = useRef<HTMLDivElement>(null);
     const evenWorks = workBlock.works.filter((_, index) => index % 2 === 0);
     const oddWorks = workBlock.works.filter((_, index) => index % 2 !== 0);
 
     useEffect(() => {
-        if (titleRef.current && descriptionRef.current) {
-            titleRef.current.style.height = (Math.round(titleRef.current.clientWidth * 9 / 16) + 60) +  'px';
+        if (titleRef.current && descriptionRef.current && borderRef.current) {
+            titleRef.current.style.height = (Math.round(titleRef.current.clientWidth * 9 / 16) + 60) + 1 + 'px';
             descriptionRef.current.style.height = (Math.round(titleRef.current.clientWidth * 9 / 16) + 60) +  'px';
+            borderRef.current.style.height = (Math.round(titleRef.current.clientWidth * 9 / 16) + 60) +  'px';
+        }
+        if (leftWorksRef.current && workGalleryRef.current){
+            workGalleryRef.current.style.height = leftWorksRef.current.clientHeight + 'px';
         }
     }, []);
 
     useEffect(() => {
         const handleScrolling = () => {
-            const workGallery = document.getElementById("workGallery");
-            if (workGallery && titleRef.current) {
-                const rect = workGallery.getBoundingClientRect();
+            if (workGalleryRef.current && titleRef.current && leftWorksRef.current) {
+                const rect = workGalleryRef.current.getBoundingClientRect();
                 const workHeight = (Math.round(titleRef.current.clientWidth * 9 / 16) + 60);
-                const scrollProgress = Math.min(1, Math.max(0, (workGallery.clientHeight - (rect.bottom - (window.innerHeight / 3))) / workGallery.clientHeight));
+                const scrollProgress = Math.min(1, Math.max(0, (workGalleryRef.current.clientHeight - (rect.bottom - (window.innerHeight / 3))) / workGalleryRef.current.clientHeight));
                 if (rightWorksRef.current) {
                     rightWorksRef.current.style.transform = `translate3d(0, ${-workHeight * scrollProgress +  'px'}, 0)`;
                 }
+                workGalleryRef.current.style.paddingTop = `${workHeight * scrollProgress +  'px'}`;
+                workGalleryRef.current.style.height = leftWorksRef.current.clientHeight + (workHeight * scrollProgress) + 'px';
             }
         }
         window.addEventListener("scroll", handleScrolling);
@@ -46,13 +54,13 @@ const WorkGallery = ({workBlock}:WorkGalleryProps) => {
     }, []);
 
     return (
-        <div className={"hidden md:flex flex-row"} id={"workGallery"}>
-            <div className={"w-1/2"}>
+        <div className={"hidden md:flex flex-row overflow-hidden"} ref={workGalleryRef}>
+            <div className={"w-1/2 h-fit"} ref={leftWorksRef}>
                 <div className={"m-0 w-full overflow-hidden border-b border-transparent flex items-center justify-center"} ref={titleRef}>
                     <h1 className={"mb-4 text-2xl lg:text-3xl"}>{workBlock.title}</h1>
                 </div>
                 {oddWorks.map((work: WorkType, index) => (
-                    <Work key={index} work={work} isOdd={true} isFirst={index === 0} isLast={index === oddWorks.length - 1 && workBlock.works.length % 2 === 0}/>
+                    <Work key={index} work={work} isOdd={true} isFirst={index === 0} isLast={index === oddWorks.length}/>
                 ))}
             </div>
             <div className={"w-1/2"} ref={rightWorksRef}>
@@ -63,6 +71,7 @@ const WorkGallery = ({workBlock}:WorkGalleryProps) => {
                     <p className={"text-sm lg:text-lg font-extralight"}>{workBlock.description}</p>
                     <CvButton cvLink={workBlock.cv}/>
                 </div>
+                <div className={'border-l border-background dark:border-text'} ref={borderRef}/>
             </div>
         </div>
     )

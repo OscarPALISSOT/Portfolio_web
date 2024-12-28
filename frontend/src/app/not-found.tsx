c63afcbb8523c82b380f5371170f48a419a6bf0e
OscarPALISSOT <oscar.p25000@gmail.com>
2024-12-28 17:35:31 +0100

    not found page

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import Link from 'next/link'
+import React from "react";
+import useCursorHandlers from "@/hooks/useCursorHandlers";
+
+export default function NotFound() {
+
+    const cursorHandlers = useCursorHandlers();
+
+    return (
+        <div className={'min-h-[calc(100vh_-_225px)] lg:min-h-[calc(100vh_-_225px)]'}>
+            <h1 className={"mt-8 mb-6 text-2xl md:text-3xl lg:text-4xl"}>404 - Not Found</h1>
+            <p className={'mb-6'}>Page inexistante</p>
+            <Link
+                className={'w-fit text-base md:text-sm lg:text-lg font-extralight border border-text p-2 lg:p-3 duration-500 ease-in-out bg-gradient-to-r from-text from-50% to-background to-50% bg-[length:200%_100%] bg-right hover:bg-left hover:text-background overflow-hidden'}
+                href={'/'}
+                target={'_blank'}
+                rel={'noreferrer'}
+                {...cursorHandlers}
+            >
+                Retour à l'accueil
+            </Link>
+        </div>
+    )
+}
